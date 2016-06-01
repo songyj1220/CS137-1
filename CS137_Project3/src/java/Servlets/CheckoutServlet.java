8baffe363ee1d46b329833173f5ed3b8cf8ccd49
songyj1220 <songyj1220@gmail.com>
2016-06-01 12:48:27 -0700

    Update CheckoutServlet.java

--- CS137_Project3/src/java/Servlets/CheckoutServlet.java
+++ CS137_Project3/src/java/Servlets/CheckoutServlet.java
@@ -35,6 +35,78 @@ public class CheckoutServlet extends HttpServlet {
         }
         return s;
     }
+    public String get_info(String eee) throws SQLException{
+        Statement statement = null;
+        Connection dbcon = null;
+        ResultSet rs = null;
+        String n = "";
+        String mail  = "no";
+        String cid ="";
+        String add = "";
+        String ct = "";
+        String st = "";
+        String zip = "";
+        String date = "";
+       List<String> list = new ArrayList<String>();
+
+       
+        
+        
+      
+        
+        try{
+                Class.forName("com.mysql.jdbc.Driver").newInstance();
+                dbcon = DriverManager.getConnection(MysqlConfig.URL, MysqlConfig.USER, MysqlConfig.PASS);
+                statement = dbcon.createStatement();
+               
+                String query = "SELECT customerID, name, email, address, city, state, zipcode, date FROM customer where email = " + "'"+ eee + "'";
+
+               rs = statement.executeQuery(query);
+                while(rs.next()){
+                  //cid = rs.getString("customerID");
+                  //n = rs.getString("name");
+                  mail = rs.getString("email");
+                  //add = rs.getString("addresss");
+                  //ct = rs.getString("city");
+                  //st = rs.getString("st");
+                  //zip = rs.getString("zipcode");
+                  //date = rs.getString("date");
+                  //list.add(cid);
+                  //list.add(n);
+                  //list.add(mail);
+                  //list.add(add);
+                  //list.add(ct);
+                  //list.add(st);
+                  //list.add(zip);
+                  //list.add(date);
+                  
+                  
+                  
+                  
+                  //names = rs.getString("name");
+                  //session.setAttribute("names", names);
+                   
+                }
+
+                rs.close();
+                statement.close();
+                dbcon.close();
+
+        }
+         catch(Exception e){
+            e.printStackTrace();
+            
+	}
+        finally {
+            if(rs !=null){
+            rs.close();}
+            if(statement != null){
+            statement.close();}
+            if(dbcon !=null){
+            dbcon.close();}
+        }
+return mail;
+    }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -111,6 +183,17 @@ public class CheckoutServlet extends HttpServlet {
                    
                         session.setAttribute("saled-items", copy);
                         session.setAttribute("current-shopping-cart", null);
+                        String eee = customer.getEmail();
+                        session.setAttribute("eee", eee);
+                        try {
+                            String mail = get_info(eee);
+                            session.setAttribute("mail", mail);
+           
+            
+            
+                            } catch (SQLException ex) {
+                            Logger.getLogger(CheckoutServlet.class.getName()).log(Level.SEVERE, null, ex);
+                            }
                    
                         String path = request.getContextPath() + "/Orderdetail";
 
